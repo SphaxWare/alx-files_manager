360bbbbfb05275d7c0943d03e537703c45197070
SphaxWare <itssphax123@gmail.com>
2024-08-03 10:49:52 -0700

    2. First API

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const AppController = require('../controllers/AppController').default;
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const routes = require('./routes/index');
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
